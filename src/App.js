b5b8c7cd6a048fc8bcbc088ba3da4088c295b62c
ObmenTrico <dimagenus@gmail.com>
2023-12-29 17:40:07 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+// const tg = window.Telegram.WebApp;
+
+function App() {
+
+
+
+    const onClose = () => {
+//        tg.close()
+    }
+
+
+  return (
+    <div className="App">
+      work
+        <button onClick={onClose}> Закрыть </button>
+    </div>
+  );
+}
+
+export default App;
