d9ba02f14db1120cfbb8d915d5e17e55b2bb0949
ObmenTrico <dimagenus@gmail.com>
2023-12-29 18:34:49 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
-// const tg = window.Telegram.WebApp;
+import {useEffect} from "react";
+
 
 function App() {
 
+    useEffect(() => {
+        tg.ready();
+    }, [])
 
 
-    const onClose = () => {
-//        tg.close()
-    }
 
 
   return (
     <div className="App">
       work
-        <button onClick={onClose}> Закрыть </button>
+
     </div>
   );
 }
