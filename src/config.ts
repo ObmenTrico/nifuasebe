2811e6c10bc764bcea54dd5e044ebb772b9bf03c
SantaSpeen <santaspeen@gmail.com>
2024-01-07 04:07:39 +0300

    change endpoint

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,3 @@
 export const config = {
-    apiEndpoint: "https://tg.anidev.ru/api"
-    // apiEndpoint: "http://91.236.198.234:8080/api" // Domain?? SSL??
+    apiEndpoint: "https://api.tg.3kk.site"
 }
