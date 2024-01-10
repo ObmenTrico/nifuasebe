22032ea36157b52ec032081bd1f5c69cbc5f2db9
SantaSpeen <santaspeen@gmail.com>
2024-01-11 01:25:13 +0300

    [~] Form to class [+] Table in Form [~] en -> ru in html [+] tokenState and userState support [+] Already ordered

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,129 +1,205 @@
-import React, {useEffect, useState} from 'react';
-import {MainButton, MainButtonProps, useShowPopup} from "@vkruglikov/react-telegram-web-app";
+import React from 'react';
+import { MainButton } from "@vkruglikov/react-telegram-web-app";
 import axios from 'axios';
 import {config} from "../config";
+import {Spin} from "antd";
 
-export const Form: React.FC = () => {
+interface FormProps {
+    fromProducts?: boolean
+    setFormOk?: React.Dispatch<React.SetStateAction<boolean>>
+    onRef?: (callback: Function) => void;
+}
+interface User {
+    name: string
+    town: string
+    street: string
+    mobile: string
+    state: number
+}
 
-    const showPopup = useShowPopup();
-    const [name, setName] = useState('');
-    const [town, setTown] = useState('');
-    const [street, setStreet] = useState('');
-    const [mobile, setMobile] = useState('');
-    const [buttonState, setButtonState] = useState<
-        { show: boolean; } & Pick<MainButtonProps, 'text' | 'progress' | 'disable'>>();
+interface State {
+    tokenOK: boolean
+    button: boolean
+    name: string
+    town: string
+    street: string
+    mobile: string
+    user: User
+}
 
-    const [userData, setUserData] = useState({
-        "name": "", "town": "", "street": "", "mobile": "", "createdAt": "", "updatedAt": ""
-    });
-    const [tokenOk, setTokenOk] = useState(true);
-    const params = new URLSearchParams(window.location.search);
-    const id = params.get('id');
-    const token = params.get('token');
-
-    const sendData = () => {
-        axios.post(
-            config.apiEndpoint + '/user/add',
-            { id: id, token: token, name: name, town: town, street: street, mobile: mobile}
-        )
-        setUserData({name: name, town: town, street: street, mobile: mobile, createdAt: '1', updatedAt: '1'})
-        setName('');
-        setTown('');
-        setStreet('');
-        setMobile('');
-        showPopup({
-            message: 'Данные приняты.',
-        })
+export class Form extends React.Component<FormProps>{
+    private id: string | null = ""
+    private token: string | null = ""
+    state: State = {
+        tokenOK: true,
+        button: false,
+        name: '',
+        town: '',
+        street: '',
+        mobile: '',
+        user: {
+            name: '',
+            town: '',
+            street: '',
+            mobile:'',
+            state: -1.
+        }
     }
 
-    useEffect(() => {
-        const checkToken = async () => {
-            try {
-                const response = await axios.post(
-                    config.apiEndpoint + '/user/check',
-                    { id: id, token: token }
-                )
-                if (response.data.error_code !== 0) {
-                    setTokenOk(false);
-                }
-            } catch (error: any) {
-                console.error('Error checking token:', error.message);
-                setTokenOk(false);
-            }
-        };
-        const downloadUser = async () => {
-            try {
-                const response = await axios.post(
-                    config.apiEndpoint + '/user/read',
-                    { id: id, token: token }
-                )
-                if (response.data.error_code === undefined) {
-                    setUserData(response.data);
-                }
-            } catch (error: any) {
-                console.error('Error checking token:', error.message);
-            }
-        };
-        checkToken().then(async () => {
-            if (tokenOk)
-                await downloadUser()
+    async componentDidMount() {
+        this.props.onRef?.(() => {
+            return this.sendData(true)
         });
-    }, []);
+        const params = new URLSearchParams(window.location.search);
+        this.id = params.get('id')
+        this.token = params.get('token')
+        // checkToken
+        await axios.post(config.apiEndpoint + '/user/check', { id: this.id, token: this.token })
+            .then(r => {
+                if (r.data.error_code === 0) {
+                    this.setState({tokenOK: false})
+                }})
+            .catch((e) => {
+                console.log("Error checkToken: ", e.message)
+                this.setState({tokenOK: false})
+            })
+        // downloadUser
+        await axios.post(config.apiEndpoint + '/user/read', { id: this.id, token: this.token })
+            .then( r => {
+                if (r.data.error_code === undefined) {
+                    this.setState({user: r.data})
+                    if (r.data.state > 0)
+                        this.props.setFormOk?.(true)
+                }
+            })
+            .catch(e => console.error('Error downloadUser:', e.message))
+    }
 
-    useEffect(() => {
-        if(name && town && street && mobile) {
-            setButtonState({show: true})
-        } else {
-            setButtonState({show: false})
+    async sendData(fromProducts: boolean) {
+        // @ts-ignore
+        const tg = window["Telegram"].WebApp
+        if (fromProducts && (!this.state.name || !this.state.town || !this.state.street || !this.state.mobile))
+            return
+        console.log("[form] sendData: fromProducts", fromProducts)
+        try {
+            const r = await axios.post(
+                config.apiEndpoint + '/user/add',
+                {id: this.id, token: this.token, name: this.state.name, town: this.state.town, street: this.state.street, mobile: this.state.mobile}
+            )
+            const code = r.data.error_code
+            if (code === 0){
+                this.setState({name: '', town: '', street: '', mobile: '', user: {
+                        name: this.state.name, town: this.state.town,
+                        street: this.state.town, mobile: this.state.mobile, state: 1
+                    }})
+                if (fromProducts)
+                    return true
+                tg.showAlert('Данные приняты')
+            } else if (code === 100) {
+                this.setState({mobile: ''})
+                tg.showAlert('Номер телефона введён неверно')
+            }
+        } catch {
+            tg.showAlert('Произошла внутреняя ошибка')
         }
-    }, [name, town, street, mobile])
+    }
 
-    return (
-        <div className={"form"}>
-            {tokenOk ? <></> : <p style={{color: "red", textAlign: "center"}}>Session not valid.</p>}
-            {userData.name ? <>
-                    <h3 style={{color: "green", textAlign: "center"}}>Вы уже ввели данные:</h3>
-                    <p>Имя: {userData.name}</p>
-                    <p>Населенный пункт: {userData.town}</p>
-                    <p>Улица: {userData.street}</p>
-                    <p>Телефона: {userData.mobile}</p>
-                    <br/>
-                    <br/>
-                    <h3 style={{textAlign: "center"}}>Введите данные</h3>
-                    <p style={{textAlign: "center"}}>Если нужно обновить</p>
-                </> :
-                <h3 style={{textAlign: "center"}}>Введите данные для связи</h3>}
-            <input
-                className={'input'}
-                type="text"
-                placeholder={'Ваше имя'}
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-            />
-            <input
-                className={'input'}
-                type="text"
-                placeholder={'Населенный пункт'}
-                value={town}
-                onChange={(e) => setTown(e.target.value)}
-            />
-            <input
-                className={'input'}
-                type="text"
-                placeholder={'Улица, номер дома и квартиры'}
-                value={street}
-                onChange={(e) => setStreet(e.target.value)}
-            />
-            <input
-                className={'input'}
-                type="text"
-                placeholder={'Номер телефона для связи'}
-                value={mobile}
-                onChange={(e) => setMobile(e.target.value)}
-            />
-            <div>{buttonState?.show ? <MainButton text={userData.name ? "Обновить" : "Отправить"} onClick={() => sendData()} {...buttonState} /> :
-                <h4 className={"underFormText"}>{userData.name ? "Что бы обновить введите все данные" : "Введите все данные что бы продолжить"}</h4>
-            }</div>
-        </div>
-    );
-};
+    buttonHandle(state: {}){
+        this.setState(state)
+        let _state = {...this.state, ...state}
+        if (!this.props.fromProducts) {
+            if(_state.name && _state.town && _state.street && _state.mobile) {
+                this.setState({button: true})
+            } else {
+                this.setState({button: false})
+            }
+        } else {
+            if(_state.name && _state.town && _state.street && _state.mobile) {
+                this.props.setFormOk?.(true)
+            } else {
+                this.props.setFormOk?.(false)
+            }
+        }
+    }
+
+    render() {
+        const { fromProducts } = this.props
+        const state = this.state.user.state
+
+        return (
+            <div className={"form"}>
+                {!this.state.tokenOK ? <></> : <p style={{color: "red", textAlign: "center"}}>Session not valid.</p>}
+                {state < 0 && <Spin fullscreen/>}
+
+                {state > 0 ?
+                    <>
+                        <h3 style={{color: "green", textAlign: "center"}}>Вы уже ввели данные:</h3>
+                        <div className={"formTable"}>
+                            <table>
+                                <tbody>
+                                <tr>
+                                    <td>Имя</td>
+                                    <td style={{ wordBreak: "break-all" }}>{this.state.user.name}</td>
+                                </tr>
+                                <tr>
+                                    <td>Адрес</td>
+                                    <td style={{ wordBreak: "break-all" }}>{this.state.user.town} {this.state.user.street}</td>
+                                </tr>
+                                <tr>
+                                    <td>Телефон</td>
+                                    <td style={{ wordBreak: "break-all" }}>{this.state.user.mobile}</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <br/>
+                        <br/>
+                        {fromProducts ?
+                            <h4 className={state > 0 ? "underFormTextGreen": "underFormText"}>{state > 0 ? "Что бы обновить введите все данные" : "Обновите данные, если нужно"}</h4> :
+                            <>
+                                <h3 style={{textAlign: "center"}}>Введите данные</h3>
+                                <p style={{textAlign: "center"}}>Если нужно обновить</p>
+                            </>
+                        }
+                    </> :
+                    <h3 style={{textAlign: "center"}}>Введите данные для связи</h3>}
+                <input
+                    className={'input'}
+                    type="text"
+                    placeholder={'Ваше имя'}
+                    value={this.state.name}
+                    onChange={(e) => this.buttonHandle({name: e.target.value})}
+                />
+                <input
+                    className={'input'}
+                    type="text"
+                    placeholder={'Населенный пункт'}
+                    value={this.state.town}
+                    onChange={(e) => this.buttonHandle({town: e.target.value})}
+                />
+                <input
+                    className={'input'}
+                    type="text"
+                    placeholder={'Улица, номер дома и квартиры'}
+                    value={this.state.street}
+                    onChange={(e) => this.buttonHandle({street: e.target.value})}
+                />
+                <input
+                    className={'input'}
+                    type="text"
+                    placeholder={'Номер телефона для связи'}
+                    value={this.state.mobile}
+                    onChange={(e) => this.buttonHandle({mobile: e.target.value})}
+                />
+                {!fromProducts && <div>
+                    {this.state.button ?
+                        <MainButton text={state > 0 ? "Обновить" : "Отправить"}
+                                    onClick={() => this.sendData(false)} /> :
+                        <h4 className={"underFormText"}>{state > 0 ? "Что бы обновить введите все данные" : "Введите все данные что бы продолжить"}</h4>
+                    }
+                </div>}
+            </div>
+        );
+    }
+
+}
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,57 +1,66 @@
-import React, { FC, useEffect, useState } from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {Button, Card, Spin} from "antd";
 import axios from "axios";
 import { config } from "../config";
 import "./Products.css"
-import {MainButton, MainButtonProps, useShowPopup} from "@vkruglikov/react-telegram-web-app";
+import {MainButton, useShowPopup} from "@vkruglikov/react-telegram-web-app";
+import {Form} from "./Form";
 
 interface Product {
-    id: number;
-    img: any;
-    title: string;
-    description: string;
-    price: number;
+    id: number
+    img: any
+    title: string
+    description: string
+    price: number
+}
+interface Cart {
+    id: number
+    title: string
+    price: number
 }
 
-const Products: FC = () => {
+const Products: React.FC = () => {
     const showPopup = useShowPopup();
-
-    // Костыль
-    const [lockSend, setLockSend] = useState(true);
-
-    const [loaded, setLoaded] = useState(false);
+    const [formOk, setFormOk] = useState(false);
+    const callbackRef = useRef<Function>();
+    const handleOnRef = (callback: Function) => { callbackRef.current = callback }
     const [products, setProducts] = useState<Product[]>([]);
-    const [tokenOk, setTokenOk] = useState(true);
-    const [cart, setCart] = useState<{ id: number; tittle: string; price: number }[]>([]);
+    const [tokenState, setTokenState] = useState(0);
+    const [userState, setUserState] = useState(-1);
+    const [cart, setCart] = useState<Cart[]>([]);
     const _params = new URLSearchParams(window.location.search);
     const id = _params.get("id");
     const token = _params.get("token");
 
     const sendData = () => {
-        axios.post(
-            config.apiEndpoint + '/cart/order',
-            {id: id, token: token
-            }).then(async () =>{
-                await showPopup({
-                    message: 'Заказ принят',
-                })
-                setCart([])
-            }).catch(async (r: any) => {
-                await showPopup({
-                    message: 'Ошибка',
-                })
-        })
+        const sd = callbackRef?.current?.()
+        console.log("[Prod] callbackRef:", sd)
+        if (sd){
+            axios.post(
+                config.apiEndpoint + '/cart/order',
+                {id: id, token: token
+                }).then(async () =>{
+                    await showPopup({
+                        message: 'Заказ принят',
+                    })
+                    setCart([])
+                }).catch(async () => {
+                    await showPopup({
+                        message: 'Ошибка',
+                    })
+            })
+        }
     }
 
     const getTotal = () => {
         return cart.reduce((total, item) => total + item.price, 0)
     }
 
-    const handleToCart = (productId: number, tittle: string, productPrice: number) => {
+    const handleToCart = (productId: number, title: string, productPrice: number) => {
         const isProductInCart = cart.some((item) => item.id === productId);
 
         if (!isProductInCart) {
-            setCart([...cart, { id: productId, tittle: tittle, price: productPrice }])
+            setCart([...cart, { id: productId, title: title, price: productPrice }])
         } else {
             const updatedCart = cart.filter((item) => item.id !== productId)
             setCart(updatedCart)
@@ -59,7 +68,7 @@ const Products: FC = () => {
     }
 
     useEffect(() => {
-        if (lockSend)
+        if (products.length === 0)
             return
         axios
             .post(config.apiEndpoint + "/cart/send", { token: token, id: id, cart: cart })
@@ -72,10 +81,25 @@ const Products: FC = () => {
 
     useEffect(() => {
         const checkToken = async () => {
-            axios
+            await axios
                 .post(config.apiEndpoint + "/user/check", { token: token, id: id })
-                .then((response) => {if (response.data.error_code !== 0) setTokenOk(false)})
-                .catch((error) => setTokenOk(false));
+                .then((response) => {
+                    if (response.data.error_code === 0){
+                        console.log("Token ok.")
+                        setTokenState(1)
+                    }
+                    else
+                        setTokenState(2)
+                }
+                )
+                .catch(() => setTokenState(2));
+            await axios.post(config.apiEndpoint + '/user/read', { token: token, id: id })
+                .then( r => {
+                    if (r.data.error_code === undefined) {
+                        setUserState(r.data.state)
+                    }
+                })
+                .catch(e => console.error('Error downloadUser:', e.message))
         };
         const loadProducts = async () => {
             axios
@@ -92,20 +116,19 @@ const Products: FC = () => {
                 .then((response) => {
                     console.log("Cart loaded:", response.data)
                     setCart(response.data)
-                    setLockSend(false)
                 })
                 .catch((error) => console.error("Error fetching cart:", error));
         }
         checkToken().then(async () => {
-            if (tokenOk) {
-                console.log("Token ok.")
-                await loadCart()
-                await loadProducts()
-                setLoaded(true)
-                console.log("Ready.")
-            }
+            await loadCart()
+            await loadProducts()
+            console.log("Ready.")
         });
     }, []);
+
+    useEffect(() => {
+        console.log("formOk", formOk)
+    }, [formOk]);
 
     function arrayBufferToBase64(buffer: any) {
         let binary = '';
@@ -119,50 +142,48 @@ const Products: FC = () => {
 
     return (
         <div className="products-list">
-            {!loaded && <Spin fullscreen></Spin>}
-            {tokenOk ? (
-                <>{
-                    products.map((product) => (
-                        <Card key={product.id} className="product-card">
-                            {/* Ваш код изображения */}
-                            <img
-                                className="product-image"
-                                src={`data:image/png;base64,${arrayBufferToBase64(product.img.data)}`}
-                                alt={product.title}
-                            />
-                            <div className="product-details">
-                                <div className="title">{product.title}</div>
-                                <div className="description">{product.description}</div>
-                                <br/>
-                                <Button
-                                    className="add-btn"
-                                    onClick={() => handleToCart(product.id, product.title, product.price)}>
-                                    {cart.some((item) => item.id === product.id) ? "Убрать" : "В корзину"}
-                                </Button>
-                                <div className="price"><b>{product.price}₽</b></div>
-                            </div>
-                        </Card>
-                    ))}
-                </>
-            ) : (
-                <p style={{ color: "red", textAlign: "center" }}>Session not valid.</p>
-            )}
-
-            {/* Общий счетчик */}
-            <div className="total-counter">
-                <h2 style={{textAlign: "center"}}>Корзина: {getTotal()}₽</h2>
-                <ul>
-                    {cart.map((item) => (
-                        <li key={item.id}>
-                            {item.tittle}: {item.price}₽
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            <div>
-                {getTotal() > 1 && <MainButton text={"Оформить заказ"} onClick={() => sendData()}/>}
-                <button onClick={() => sendData()}>Оформить заказ</button>
-            </div>
+            {products.length === 0 && <Spin fullscreen/>}
+            {tokenState === 0 && <p style={{color: "green", textAlign: "center"}}>Загрузка...</p>}
+            {tokenState === 2 && <p style={{color: "red", textAlign: "center"}}>Session not valid.</p>}
+            {tokenState === 1 && userState !== 2 && <>{
+                products.map((product) => (
+                    <Card key={product.id} className="product-card">
+                        <img
+                            className="product-image"
+                            src={`data:image/png;base64,${arrayBufferToBase64(product.img.data)}`}
+                            alt={product.title}
+                        />
+                        <div className="product-details">
+                            <div className="title">{product.title}</div>
+                            <div className="description">{product.description}</div>
+                            <br/>
+                            <Button
+                                className="add-btn"
+                                onClick={() => handleToCart(product.id, product.title, product.price)}>
+                                {cart.some((item) => item.id === product.id) ? "Убрать" : "В корзину"}
+                            </Button>
+                            <div className="price"><b>{product.price}₽</b></div>
+                        </div>
+                    </Card>
+                ))}
+                <div className="total-counter">
+                    <h2 style={{textAlign: "center"}}>Корзина: {getTotal()}₽</h2>
+                    <ul>
+                        {cart.map((item) => (
+                            <li key={item.id}>
+                                {item.title}: {item.price}₽
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <Form fromProducts setFormOk={setFormOk} onRef={handleOnRef}/>
+                <div>
+                    {getTotal() > 1 && formOk && <MainButton text={"Оформить заказ"} onClick={() => sendData()}/>}
+                </div>
+            </>}
+            {tokenState === 1 && userState === 2 && <>
+                <p>Вы уже сделали заказ</p>
+            </>}
         </div>
     );
 };
